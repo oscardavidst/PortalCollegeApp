a695c6874132ab28cf67553d4ac3cd7ffae5e8b3
Oscar David Soto Tellez <osoto@asesoftware.com>
2025-03-27 05:31:06 -0500

    courses

--- src/app/courses/interfaces/response-course.interface.ts
+++ src/app/courses/interfaces/response-course.interface.ts
@@ -1,4 +1,4 @@
-export interface ResponseProfessor {
+export interface ResponseCourse {
   id: number;
   name: string;
   credits: number;
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -4,53 +4,34 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, Observable, throwError } from 'rxjs';
 
 import { ResponseApi } from '../../core/interfaces/response.interface';
-import { ResponseProfessor } from '../interfaces/response-professor.interface';
-import { PostProfessor } from '../interfaces/post-professor.interface';
-import { PatchProfessor } from '../interfaces/patch-professor.interface';
+import { ResponseCourse } from '../interfaces/response-course.interface';
+import { PostCourse } from '../interfaces/post-course.interface';
+import { PatchCourse } from '../interfaces/patch-course.interface';
 
 const baseUrl = environment.baseUrl;
 
 @Injectable({
   providedIn: 'root',
 })
-export class ProfessorsService {
+export class CoursesService {
   private http = inject(HttpClient);
 
-  getProfessors(
+  getCourses(
     name: string,
     lastName: string
-  ): Observable<ResponseApi<ResponseProfessor[]>> {
+  ): Observable<ResponseApi<ResponseCourse[]>> {
     const nameUrl = name ? `Name=${name}` : '';
     const lastNameUrl = lastName ? `Name=${lastName}` : '';
     let parameters: string = [nameUrl, lastNameUrl].filter((x) => x).join('&');
     parameters = parameters.length ? `?${parameters}` : '';
-    const url = `${baseUrl}/Professors${parameters}`;
+    const url = `${baseUrl}/Courses${parameters}`;
 
     const headers = new HttpHeaders().set(
       'Authorization',
       `Bearer ${localStorage.getItem('token')}`
     );
 
-    return this.http
-      .get<ResponseApi<ResponseProfessor[]>>(url, { headers })
-      .pipe(
-        map((resp) => {
-          return resp;
-        }),
-        catchError((error) => {
-          return throwError(() => error.error.Message);
-        })
-      );
-  }
-
-  getProfessor(id: number): Observable<ResponseApi<ResponseProfessor>> {
-    const url = `${baseUrl}/Professors/${id}`;
-    const headers = new HttpHeaders().set(
-      'Authorization',
-      `Bearer ${localStorage.getItem('token')}`
-    );
-
-    return this.http.get<ResponseApi<ResponseProfessor>>(url, { headers }).pipe(
+    return this.http.get<ResponseApi<ResponseCourse[]>>(url, { headers }).pipe(
       map((resp) => {
         return resp;
       }),
@@ -60,15 +41,32 @@ export class ProfessorsService {
     );
   }
 
-  postProfessor(professor: PostProfessor): Observable<ResponseApi<number>> {
-    const url = `${baseUrl}/Professors`;
+  getCourse(id: number): Observable<ResponseApi<ResponseCourse>> {
+    const url = `${baseUrl}/Courses/${id}`;
+    const headers = new HttpHeaders().set(
+      'Authorization',
+      `Bearer ${localStorage.getItem('token')}`
+    );
+
+    return this.http.get<ResponseApi<ResponseCourse>>(url, { headers }).pipe(
+      map((resp) => {
+        return resp;
+      }),
+      catchError((error) => {
+        return throwError(() => error.error.Message);
+      })
+    );
+  }
+
+  postCourse(course: PostCourse): Observable<ResponseApi<number>> {
+    const url = `${baseUrl}/Courses`;
     const headers = new HttpHeaders().set(
       'Authorization',
       `Bearer ${localStorage.getItem('token')}`
     );
     const body = {
-      name: professor.name,
-      lastName: professor.lastName,
+      name: course.name,
+      credits: course.credits,
     };
 
     return this.http.post<ResponseApi<number>>(url, body, { headers }).pipe(
@@ -81,16 +79,16 @@ export class ProfessorsService {
     );
   }
 
-  patchProfessor(professor: PatchProfessor): Observable<ResponseApi<number>> {
-    const url = `${baseUrl}/Professors/`;
+  patchCourse(course: PatchCourse): Observable<ResponseApi<number>> {
+    const url = `${baseUrl}/Courses/`;
     const headers = new HttpHeaders().set(
       'Authorization',
       `Bearer ${localStorage.getItem('token')}`
     );
     const body: { [key: string]: string | number } = {
-      ...(professor.id && { id: professor.id }),
-      ...(professor.name && { name: professor.name }),
-      ...(professor.lastName && { lastName: professor.lastName }),
+      ...(course.id && { id: course.id }),
+      ...(course.name && { name: course.name }),
+      ...(course.credits && { credits: course.credits }),
     };
 
     return this.http.patch<ResponseApi<number>>(url, body, { headers }).pipe(
@@ -103,8 +101,8 @@ export class ProfessorsService {
     );
   }
 
-  deleteProfessor(id: number): Observable<ResponseApi<number>> {
-    const url = `${baseUrl}/Professors/${id}`;
+  deleteCourse(id: number): Observable<ResponseApi<number>> {
+    const url = `${baseUrl}/Courses/${id}`;
     const headers = new HttpHeaders().set(
       'Authorization',
       `Bearer ${localStorage.getItem('token')}`
